7f5e0fc7ea30022d8411f0b61aaa8ab13e9c531a
Sushil Subedi <sushil@truemark.com.np>
2022-07-11 22:45:46 +0545

    remove unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import CustomSlider from './components/Slider';
 import './App.css';
-import Table from './components/Table';
 
 const App = () => {
   return (
     <div className="w-100 h-100">
       <CustomSlider />
-      <Table />
     </div>
   );
 };
